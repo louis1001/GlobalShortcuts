0328aab8e1ed90351e659f16fd37389400178074
louis1001 <luisal1207@gmail.com>
2019-06-08 14:22:41 -0600

    Initial Commit

---
+++ GShortcuts/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  GShortcuts
+//
+//  Created by Luis Gonzalez on 6/8/19.
+//  Copyright © 2019 Luis Gonzalez. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
